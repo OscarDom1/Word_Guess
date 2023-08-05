8e95ec9c9fd3004f8ec0d618b959a0d6e03c96c9
Chinedu Asogwa <111493495+OscarDom1@users.noreply.github.com>
2023-08-05 10:11:10 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import "./App.css";
+import Board from "./component/Board";
+import Keyboard from "./component/Keyboard";
+import { boardDefault, generateWordSet } from "./Words";
+import { createContext, useState, useEffect } from "react";
+
+export const AppContext = createContext();
+
+function App() {
+  const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
+  const [wordSet, setWordSet] = useState(new Set());
+  const [disabledLetters, setDisabledLetters] = useState([]);
+
+  const correctWord = "RIGHT";
+
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      setWordSet(words.wordSet);
+    });
+  }, []);
+
+  const onSelectLetter = (keyVal) => {
+    if (currAttempt.letterPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
+  };
+
+  const onDelete = () => {
+    if (currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos - 1 });
+  };
+
+  const onEnter = () => {
+
+    if (currAttempt.letterPos !== 5) return;
+    let currWord = "";
+    for (let i = 0; i < 5; i++){
+      currWord += board[currAttempt.attempt][i];
+    }
+    if (wordSet.has(currWord.toLowerCase())){
+    setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+
+    }else{
+      alert("Word Not Found");
+    }
+    if (currWord === correctWord){
+      alert("Game Ended");
+    }
+  };
+  return (
+    <div className="App">
+      <nav>
+        <h1>Words-Game</h1>
+      </nav>
+
+      <AppContext.Provider
+        value={{
+          board,
+          setBoard,
+          currAttempt,
+          setCurrAttempt,
+          onSelectLetter,
+          onDelete,
+          onEnter,
+          correctWord,
+          disabledLetters, setDisabledLetters
+        }}
+      >
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
+      </AppContext.Provider>
+    </div>
+  );
+}
+
+export default App;
